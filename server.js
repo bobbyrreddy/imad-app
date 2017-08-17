803244642bd62b0cadcc7e0e22c00f81aa063baa
Bharat Reddy <bobbyrreddy@gmail.com>
2017-08-18 01:09:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.get('/ui/article-one', function (req,res) {
   res.sendFile(path.join(__dirname, 'ui', "article-one.html"));
 });
 
-app.get('/article-two',function (req,res){
+app.get('/ui/article-two',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', "article-two.html"));
 });
 
-app.get('/article-three',function (req,res){
+app.get('/ui/article-three',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', "article-three.html"));
 });
 
